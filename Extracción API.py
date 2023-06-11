f9279e2c9552576dd9862df913606be989520c4d
fdelorenzi98 <132779298+fdelorenzi98@users.noreply.github.com>
2023-06-11 20:16:51 -0300

    Update Extracción API.py

--- Extracción API.py
+++ Extracción API.py
@@ -1,10 +1,13 @@
+import pandas as pd
 import requests
-import json
+
 url = 'https://apis.datos.gob.ar/series/api/series?ids=168.1_T_CAMBIOR_D_0_0_26&limit=1000&sort=desc&format=json'
 
-response = requests.get(url,timeout=1)
+response = requests.get(url, timeout=1)
 
 if response.status_code == 200:
     data = response.json()
+    df = pd.DataFrame(data['data'])
+    print(df)
 else:
     print('Error en la solicitud:', response.status_code)
