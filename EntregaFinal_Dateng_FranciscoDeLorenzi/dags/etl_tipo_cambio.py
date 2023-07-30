fed3940a4342a90c75d0bdeb5ac2863438df9411
fdelorenzi98 <132779298+fdelorenzi98@users.noreply.github.com>
2023-07-30 10:21:15 -0300

    Creación carpeta entrega final

---
+++ EntregaFinal_Dateng_FranciscoDeLorenzi/dags/etl_tipo_cambio.py
@@ -0,0 +1,86 @@
+# Este es el DAG que orquesta el ETL de la tabla tipo_cambio
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.models import Variable
+from datetime import datetime, timedelta
+
+# Crear la tabla en Amazon Redshift
+TABLE_NAME = "tipo_cambio"
+SCHEMA="franciscodlorenzi_coderhouse"
+QUERY_CREATE_TABLE = """
+    CREATE TABLE IF NOT EXISTS {}.{} (
+        Fecha date DISTKEY,
+        Cambio double precision,
+        Aumento_porcentual double precision
+        process_date VARCHAR(10) DISTKEY
+    )SORTKEY (Fecha);
+""".format(SCHEMA, TABLE_NAME)
+
+QUERY_CLEAN_PROCESS_DATE = """
+DELETE FROM {}.{} WHERE process_date = '{{ ti.xcom_pull(key="process_date") }}';
+""".format(SCHEMA, TABLE_NAME)
+
+# Crear función para obtener process_date y enviarlo a xcom
+def get_process_date(**kwargs):
+    # Si se proporciona la fecha del proceso se toma, sino se elige la fecha de hoy
+    if (
+        "process_date" in kwargs["dag_run"].conf
+        and kwargs["dag_run"].conf["process_date"] is not None
+    ):
+        process_date = kwargs["dag_run"].conf["process_date"]
+    else:
+        process_date = kwargs["dag_run"].conf.get(
+            "process_date", datetime.now().strftime("%Y-%m-%d")
+        )
+    kwargs["ti"].xcom_push(key="process_date", value=process_date)
+
+
+defaul_args = {
+    "owner": "Francisco De Lorenzi",
+    "start_date": datetime(2023, 7, 20),
+    "retries": 0,
+    "retry_delay": timedelta(seconds=5),
+    "catchup": False,
+}
+
+with DAG(
+    dag_id="etl_tipo_cambio",
+    default_args=defaul_args,
+    description="ETL de la tabla tipo_cambio",
+    schedule_interval="@daily",
+) as dag:
+    
+    # Tareas
+    get_process_date_task = PythonOperator(
+        task_id="get_process_date",
+        python_callable=get_process_date,
+        provide_context=True,
+        dag=dag,
+    )
+
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="redshift_default",
+        sql=QUERY_CREATE_TABLE,
+        dag=dag,
+    )
+
+    clean_process_date = SQLExecuteQueryOperator(
+        task_id="clean_process_date",
+        conn_id="redshift_default",
+        sql=QUERY_CLEAN_PROCESS_DATE,
+        dag=dag,
+    )
+
+    spark_etl_tipo_cambio = SparkSubmitOperator(
+        task_id="spark_etl_tipo_cambio",
+        application=f'{Variable.get("spark_scripts_dir")}/ETL_tipo_cambio.py',
+        conn_id="spark_default",
+        dag=dag,
+        driver_class_path=Variable.get("driver_class_path"),
+    )
+
+    get_process_date_task >> create_table >> clean_process_date >> spark_etl_tipo_cambio
---
+++ EntregaFinal_Dateng_FranciscoDeLorenzi/scripts/ETL_tipo_cambio.py
@@ -0,0 +1,80 @@
+# Este script se desarrolla en Spark y realiza el proceso de ETL de la tabla tipo_cambio
+
+import requests
+from os import environ as env
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, expr, pct_change
+from pyspark.sql.types import DoubleType, DateType
+from pyspark.sql.functions import concat, col, lit, when, expr, to_date
+from commons import ETL_Spark
+
+class ETL_tipo_cambio(ETL_Spark):
+    def __init__(self, job_name=None):
+        super().__init__(job_name)
+        self.process_date = datetime.now().strftime("%Y-%m-%d")
+
+    def run(self):
+        process_date = "2023-07-20"  # datetime.now().strftime("%Y-%m-%d")
+        self.execute(process_date)
+
+    def extract(self):
+        """
+        Extrae datos de la API y transformación
+        """
+        print(">>> [E] Extrayendo datos de la API...")
+
+        # Crear una instancia de SparkSession
+        spark = SparkSession.builder.getOrCreate()
+
+        # Obtener los datos de la API y cargarlos en un DataFrame
+        url = 'https://apis.datos.gob.ar/series/api/series?ids=168.1_T_CAMBIOR_D_0_0_26&limit=1000&sort=desc&format=json'
+        response = requests.get(url, timeout=50)
+
+        if response.status_code == 200:
+            data = response.json()
+            data_list = data['data']
+
+            # Crear un DataFrame de Spark a partir de la lista de datos
+            df = spark.createDataFrame(data_list)
+
+            # Asignar nombres a las columnas
+            df = df.withColumnRenamed("indice_tiempo", "fecha").withColumnRenamed("valor", "cambio")
+
+            # Convertir la columna 'fecha' a tipo DateType
+            df = df.withColumn("fecha", col("fecha").cast(DateType()))
+
+            # Eliminar filas con fecha o cambio nulo
+            df = df.dropna(subset=["fecha", "cambio"])
+
+            # Ordenar los datos por fecha en orden ascendente
+            df = df.orderBy("fecha")
+
+            # Calcular el aumento porcentual diario del tipo de cambio
+            df = df.withColumn("aumento_porcentual", pct_change("cambio").over(Window.orderBy("fecha")) * 100)
+
+        else:
+            print('Error en la solicitud:', response.status_code, '. No se procedió a la carga de datos en Redshift.')
+
+    def load(self, df):
+        """
+        Carga los datos transformados en Redshift
+        """
+        # add process_date column
+        df = df.withColumn("process_date", lit(self.process_date))
+
+        df.write \
+            .format("jdbc") \
+            .option("url", env['REDSHIFT_URL']) \
+            .option("dbtable", f"{env['REDSHIFT_SCHEMA']}.users") \
+            .option("user", env['REDSHIFT_USER']) \
+            .option("password", env['REDSHIFT_PASSWORD']) \
+            .option("driver", "org.postgresql.Driver") \
+            .mode("append") \
+            .save()
+            
+        print("El proceso de carga de datos ha finalizado correctamente.")
+
+if __name__ == "__main__":
+    print("Corriendo script")
+    etl = ETL_tipo_cambio()
+    etl.run()
